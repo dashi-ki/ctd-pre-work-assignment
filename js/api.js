1f83a7bd833fa64fb6ad309b43b2eb3907197281
Daria T <dtolkachova@protonmail.com>
2024-07-25 12:32:43 -0400

    add random images with breeds endpoint for quiz

--- js/api.js
+++ js/api.js
@@ -1,11 +1,32 @@
 console.log("api is loaded");
+// REPLACE PLACEHOLDER WITH VALIE API KEY
+const apiKey = "live_XXXXXX";
+
 async function fetchBreeds() {
   try {
-    const response = await fetch("https://api.thedogapi.com/v1/breeds");
+    const response = await fetch("https://api.thedogapi.com/v1/breeds", {
+      headers: { "x-api-key": apiKey },
+    });
     const data = response.json();
     return data;
   } catch (error) {
     console.log("Error Fetching Breeds Info");
+    return [];
+  }
+}
+
+async function fetchBreedForQuiz() {
+  try {
+    const response = await fetch(
+      "https://api.thedogapi.com/v1/images/search?has_breeds=true&limit=3",
+      {
+        headers: { "x-api-key": apiKey },
+      }
+    );
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log("Error Fetching Random Breeds with Images");
     return [];
   }
 }
